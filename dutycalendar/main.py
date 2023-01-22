4321b441488eab6e5cfb4d161e9e0e363e8aad52
Joris <jorisbeets@icloud.com>
2023-01-22 19:27:17 +0100

    schedule.py has the startdates working for two sch

--- dutycalendar/main.py
+++ dutycalendar/main.py
@@ -4,10 +4,32 @@
 # modules ics, UUID, maybe dateutil.
 
 
-from ics import Calendar, Event 
+from datetime import date, timedelta
 
-c = Calendar()
-e = Event()
-e.name ="test"
+from ics import Calendar, Event
 
-print(e)
+from schedule import Schedule
+
+
+def main()->None:
+    # instanciate Schedules with startdates
+    # . 
+    joris = Schedule(name = "JorisSched",days_on =[15],days_off = [20],startdate=[date(2023,1,17)],counter_length = 11)
+    caroline = Schedule(name = "caroline",days_on =[8,9],days_off = [22,21],startdate=[date(2022,12,26),date(2022,12,10)],counter_length = 10)
+    
+    # Create lists with the startdates
+    joris.start_dates(count=joris.counter_length)
+    caroline.start_dates(count=caroline.counter_length)
+
+    #create calendar: Kan evt ook in een andere file.
+    c = Calendar()
+    for date in joris.startdates:
+        i = 0
+        e= Event()
+        e.description = f"{joris.name} {i}"
+        e.begin = date
+        e.duration = timedelta(days=joris.days_on) #nu moet ik om en om de kort en lange hebben?????
+    
+
+if __name__ == "__main__":
+    main()
--- dutycalendar/schedule.py
+++ dutycalendar/schedule.py
@@ -1,21 +1,45 @@
 """
-schedule.py contains the class Schedule. 
-Every Schedule has:
-name
-days on
-days off
-startdate
-counter_length
+Explain class
 """
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from datetime import date 
 
+from dateutil.rrule import rrule,rruleset, DAILY
 
 @dataclass
-class Schedule():
-    name = str
-    days_on = int
-    days_off = int
-    startdate = date
-    counter_length = int
+class Schedule:
+    # Explain class variables
+    name: str
+    counter_length: int = field(repr=False)# gedaan om de dataclass mogelijkheden te laten zien.(fields)
+    startdate: list =field(default=list)
+    days_on: list = field(default_factory=list)
+    days_off: list = field(default_factory=list) 
+    startdates: list = field(default_factory=list)
+
+    def start_dates(self, count:int) -> list:
+        # Create rruleset() that has two rule if there are two items in the days_off list. Can still be modified to accomodate flexing to len of list.
+        # for i in range(len(days_off):  etc etc etc)
+        self.startdates = rruleset() # DE rruleset verzamelt rrules
+        # Rrule No:1 deze wordt altijd gedaan
+        self.startdates.rrule(rrule(
+            freq=DAILY,
+            dtstart=self.startdate[0],
+            count =count,#deze kan ook met until gedaan.
+            interval=(self.days_off[0]+self.days_on[0])
+            ))
+        if len(self.days_off) > 1:
+             # RRule No2: deze wordt gedaan als de lijsten langer zijn dan 1.
+            self.startdates.rrule(rrule(
+                freq=DAILY,
+                dtstart=self.startdate[1],
+                count =count,#deze kan ook met until gedaan.
+                interval=(self.days_off[1]+self.days_on[1])
+                ))
+        
+        return self.startdates
+    
+"""
+    Elke schedule creeert een dateutil rruleset die als er twee schedules zijn samengevoegd
+    worden met een rrule set. die daarna pas aan de calendar togevoegd wordt. 
+"""
 
